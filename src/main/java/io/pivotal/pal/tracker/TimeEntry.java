907c265bbb1690d0f7618e5b026b7913a588cd18
Alagu Sundaram <alagusundaramswaminathan@gmail.com>
2017-12-05 18:35:01 -0500

    spring mvc application added

--- src/main/java/io/pivotal/pal/tracker/TimeEntry.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntry.java
@@ -1,7 +1,5 @@
 package io.pivotal.pal.tracker;
-
 import java.time.LocalDate;
-import java.util.Objects;
 
 public class TimeEntry {
     private long id;
@@ -10,6 +8,15 @@ public class TimeEntry {
     private LocalDate date;
     private int hours;
 
+    public TimeEntry() {
+    }
+
+    public TimeEntry(long projectId, long userId, LocalDate date, int hours) {
+        this.projectId = projectId;
+        this.userId = userId;
+        this.date = date;
+        this.hours = hours;
+    }
 
     public TimeEntry(long id, long projectId, long userId, LocalDate date, int hours) {
         this.id = id;
@@ -18,7 +25,7 @@ public class TimeEntry {
         this.date = date;
         this.hours = hours;
     }
-    
+
     public long getId() {
         return id;
     }
@@ -31,50 +38,40 @@ public class TimeEntry {
         return projectId;
     }
 
-    public void setProjectId(long projectId) {
-        this.projectId = projectId;
-    }
-
     public long getUserId() {
         return userId;
-    }
-
-    public void setUserId(long userId) {
-        this.userId = userId;
     }
 
     public LocalDate getDate() {
         return date;
     }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
     public int getHours() {
         return hours;
-    }
-
-    public void setHours(int hours) {
-        this.hours = hours;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof TimeEntry)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
+
         TimeEntry timeEntry = (TimeEntry) o;
-        return getId() == timeEntry.getId() &&
-                getProjectId() == timeEntry.getProjectId() &&
-                getUserId() == timeEntry.getUserId() &&
-                getHours() == timeEntry.getHours() &&
-                Objects.equals(getDate(), timeEntry.getDate());
+
+        if (id != timeEntry.id) return false;
+        if (projectId != timeEntry.projectId) return false;
+        if (userId != timeEntry.userId) return false;
+        if (hours != timeEntry.hours) return false;
+        return date != null ? date.equals(timeEntry.date) : timeEntry.date == null;
     }
 
     @Override
     public int hashCode() {
-
-        return Objects.hash(getId(), getProjectId(), getUserId(), getDate(), getHours());
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (int) (projectId ^ (projectId >>> 32));
+        result = 31 * result + (int) (userId ^ (userId >>> 32));
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        result = 31 * result + hours;
+        return result;
     }
 
     @Override
@@ -83,7 +80,7 @@ public class TimeEntry {
                 "id=" + id +
                 ", projectId=" + projectId +
                 ", userId=" + userId +
-                ", date=" + date +
+                ", date='" + date + '\'' +
                 ", hours=" + hours +
                 '}';
     }
